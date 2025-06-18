efb67dab8664cef29c40ad13b4aa39deb1e2eca2
EnzoDaviSG <166147810+EnzoDaviSG@users.noreply.github.com>
2025-06-18 18:12:49 -0300

    Add files via upload

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-    } else {
-        sidebar.style.width = '250px';
-    }
-}
